92ba86f77f0405a0953b18a4fb69d2b1bc327c13
Andrew H. Yi <andrew.yi50@gmail.com>
2015-04-19 00:57:22 -0400

    User can delete meme from table view

--- MemeMate/MemeTableViewController.swift
+++ MemeMate/MemeTableViewController.swift
@@ -75,51 +75,19 @@ class MemeTableViewController: UITableViewController {
         navigationController?.title = "Resend Meme"
         navigationController?.pushViewController(editViewController, animated: true)
     }
-    /*
-    // Override to support conditional editing of the table view.
+    
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
-    // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
-            // Delete the row from the data source
+            (UIApplication.sharedApplication().delegate as! AppDelegate).savedMemes.removeAtIndex(indexPath.row)
+            memes = (UIApplication.sharedApplication().delegate as! AppDelegate).savedMemes
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-    
     // MARK: Button Actions
     func showEditView() {
         let editViewController = EditMemeViewController()
